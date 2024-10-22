450d627cdaab53f90f4871140d0c1b65f8edb8ea
Ricardo Conceição <ricardo.conceicao@cosm.com>
2024-10-22 19:15:34 -0300

    feat(FS-2): add domain entity Product and unit test

--- internal/core/domain/entity/product_test.go
+++ internal/core/domain/entity/product_test.go
@@ -7,11 +7,45 @@ import (
 )
 
 func TestProduct_Activate(t *testing.T) {
-  p := Product{
-    CreatedAt: time.Now(),
-    UpdatedAt: time.Now(),
-  }
-  err := p.Activate()
-  require.Error(t, err)
-  require.EqualError(t, err, "product already active")
+  t.Run("product is inactive", func(t *testing.T) {
+    p := Product{
+      CreatedAt: time.Now(),
+      UpdatedAt: time.Now(),
+      DeletedAt: time.Now(),
+    }
+    err := p.Activate()
+    require.NoError(t, err)
+    require.True(t, p.IsActive())
+  })
+  t.Run("product is active", func(t *testing.T) {
+    p := Product{
+      CreatedAt: time.Now(),
+      UpdatedAt: time.Now(),
+    }
+    err := p.Activate()
+    require.Error(t, err)
+    require.EqualError(t, err, "product already active")
+  })
+}
+
+func TestProduct_Inactivate(t *testing.T) {
+  t.Run("product is active", func(t *testing.T) {
+    p := Product{
+      CreatedAt: time.Now(),
+      UpdatedAt: time.Now(),
+    }
+    err := p.Inactivate()
+    require.NoError(t, err)
+    require.False(t, p.IsActive())
+  })
+  t.Run("product is inactive", func(t *testing.T) {
+    p := Product{
+      CreatedAt: time.Now(),
+      UpdatedAt: time.Now(),
+      DeletedAt: time.Now(),
+    }
+    err := p.Inactivate()
+    require.Error(t, err)
+    require.EqualError(t, err, "product already inactive")
+  })
 }
