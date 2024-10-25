92170387d37375d4525bf93fab6e4cb01f555f1a
Ricardo Conceição <ricardo.conceicao@cosm.com>
2024-10-25 14:23:36 -0300

    feat(FS-2): add category serice implementation

---
+++ internal/core/service/category.go
@@ -0,0 +1,71 @@
+package service
+
+import (
+  "context"
+  "github.com/vitovidale/TECH-CHALLENGE/internal/core/domain"
+  "github.com/vitovidale/TECH-CHALLENGE/internal/core/port"
+)
+
+type CategoryService struct {
+  categoryRepository port.CategoryRepository
+}
+
+func NewCategoryService(categoryRepository port.CategoryRepository) *CategoryService {
+  return &CategoryService{categoryRepository: categoryRepository}
+}
+
+func (s *CategoryService) GetByID(ctx context.Context, id int) (*domain.Category, error) {
+  c, err := s.categoryRepository.FindCategoryByID(ctx, id)
+  if err != nil {
+    return nil, err
+  }
+  return c, nil
+}
+
+func (s *CategoryService) GetAll(ctx context.Context) ([]*domain.Category, error) {
+  c, err := s.categoryRepository.FindAllCategories(ctx)
+  if err != nil {
+    return nil, err
+  }
+  return c, nil
+}
+
+func (s *CategoryService) Create(ctx context.Context, c *domain.Category) (*domain.Category, error) {
+  err := s.categoryRepository.Create(ctx, c)
+  if err != nil {
+    return nil, err
+  }
+  return c, nil
+}
+
+func (s *CategoryService) Inactivate(ctx context.Context, id int) (*domain.Category, error) {
+  c, err := s.categoryRepository.FindCategoryByID(ctx, id)
+  if err != nil {
+    return nil, err
+  }
+  err = c.Inactivate()
+  if err != nil {
+    return nil, err
+  }
+  err = s.categoryRepository.Update(ctx, c)
+  if err != nil {
+    return nil, err
+  }
+  return c, nil
+}
+
+func (s *CategoryService) Activate(ctx context.Context, id int) (*domain.Category, error) {
+  c, err := s.categoryRepository.FindCategoryByID(ctx, id)
+  if err != nil {
+    return nil, err
+  }
+  err = c.Activate()
+  if err != nil {
+    return nil, err
+  }
+  err = s.categoryRepository.Update(ctx, c)
+  if err != nil {
+    return nil, err
+  }
+  return c, nil
+}
